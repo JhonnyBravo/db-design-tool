3ed7fb1c7f2a5681a22f278a6f09b72b20abda02
Jhonny Bravo <sanfranceshika5@gmail.com>
2020-01-17 18:26:28 +0900

    new file: src/main/java/db_design_tool/app/filter/ContentTypeFilter.java

---
+++ src/main/java/db_design_tool/app/filter/ContentTypeFilter.java
@@ -0,0 +1,50 @@
+package db_design_tool.app.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class ContentTypeFilter
+ */
+@WebFilter("/*")
+public class ContentTypeFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public ContentTypeFilter() {
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
