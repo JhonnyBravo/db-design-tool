dd89fc1159ffe5ffcabb9fc63d23557506f4a269
JhonnyBravo <sanfranceshika5@gmail.com>
2020-01-22 21:42:23 +0900

    modified: src/test/resources/init_db_design_tool.sql
    new table: field_master

--- src/test/resources/init_db_design_tool.sql
+++ src/test/resources/init_db_design_tool.sql
@@ -9,4 +9,17 @@ CREATE TABLE IF NOT EXISTS table_master (
     PRIMARY KEY (table_id)
 );
 
---grant all privileges on db_design_tool.* to user@localhost;
+CREATE TABLE IF NOT EXISTS field_master (
+    table_id INT NOT NULL,
+    field_id INT AUTO_INCREMENT,
+    no INT NOT NULL,
+    physical_name VARCHAR(100) NOT NULL,
+    logical_name VARCHAR(100) NOT NULL,
+    data_type VARCHAR(10) NOT NULL,
+    data_size INT,
+    description VARCHAR(255),
+    delete_flag INT NOT NULL,
+    PRIMARY KEY (field_id)
+);
+
+-- grant all privileges on db_design_tool.* to user@localhost;
