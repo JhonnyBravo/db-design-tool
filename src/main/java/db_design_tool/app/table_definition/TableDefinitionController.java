46aca091cdf66adfe7bb4ea8a91afacd5de4c41f
JhonnyBravo <sanfranceshika5@gmail.com>
2020-02-15 14:06:26 +0900

    modified: src/main/java/db_design_tool/app/table_definition/TableDefinitionController.java
    フォームから受け取った入力値をモデルに変換する処理と
    エラーメッセージを設定する処理を追加。

--- src/main/java/db_design_tool/app/table_definition/TableDefinitionController.java
+++ src/main/java/db_design_tool/app/table_definition/TableDefinitionController.java
@@ -1,6 +1,7 @@
 package db_design_tool.app.table_definition;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -9,6 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import db_design_tool.domain.model.FieldMaster;
+import db_design_tool.domain.model.TableDefinition;
+import db_design_tool.domain.model.TableMaster;
 
 /**
  * Servlet implementation class TableDefinitionController
@@ -17,11 +25,14 @@ import javax.servlet.http.HttpServletResponse;
 public class TableDefinitionController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    private final TableDefinitionHelper helper;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public TableDefinitionController() {
         super();
+        helper = new TableDefinitionHelper();
     }
 
     /**
@@ -44,7 +55,66 @@ public class TableDefinitionController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+        final TableMaster tableMaster = helper.convertToTableMaster(request);
+
+        final FieldMaster[] fieldMasterArray = helper
+                .convertToFieldMaster(request);
+
+        final TableDefinition tableDefinition = new TableDefinition();
+        tableDefinition.setTableMaster(tableMaster);
+        tableDefinition.setFieldMaster(fieldMasterArray);
+
+        final Validator validator = Validation.buildDefaultValidatorFactory()
+                .getValidator();
+        boolean hasError = false;
+
+        final Set<ConstraintViolation<TableMaster>> tableMasterViolation = validator
+                .validate(tableMaster);
+
+        if (tableMasterViolation.size() > 0) {
+            hasError = true;
+
+            tableMasterViolation.stream().forEach(e -> {
+                final String path = e.getPropertyPath().toString();
+
+                if (path.equals("physicalName")) {
+                    tableMaster.setPhysicalNameError(e.getMessage());
+                } else if (path.equals("logicalName")) {
+                    tableMaster.setLogicalNameError(e.getMessage());
+                }
+            });
+        }
+
+        request.setAttribute("tableMaster", tableMaster);
+
+        if (fieldMasterArray != null) {
+            for (final FieldMaster fieldMaster : fieldMasterArray) {
+                final Set<ConstraintViolation<FieldMaster>> fieldMasterViolation = validator
+                        .validate(fieldMaster);
+
+                if (fieldMasterViolation.size() > 0) {
+                    hasError = true;
+
+                    fieldMasterViolation.stream().forEach(e -> {
+                        final String path = e.getPropertyPath().toString();
+
+                        if (path.equals("physicalName")) {
+                            fieldMaster.setPhysicalNameError(e.getMessage());
+                        } else if (path.equals("logicalName")) {
+                            fieldMaster.setLogicalNameError(e.getMessage());
+                        } else if (path.equals("description")) {
+                            fieldMaster.setDescriptionError(e.getMessage());
+                        }
+                    });
+                }
+            }
+        } else {
+            hasError = true;
+            request.setAttribute("fieldMasterError", "フィールド定義を入力してください。");
+        }
+
+        request.setAttribute("fieldMasterArray", fieldMasterArray);
+
         doGet(request, response);
     }
 
