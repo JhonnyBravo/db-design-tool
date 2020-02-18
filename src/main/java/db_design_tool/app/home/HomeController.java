b897ce96d45fc9a65199fb3e2418aeecf0dc4c80
JhonnyBravo <sanfranceshika5@gmail.com>
2020-02-18 22:02:11 +0900

    modified: src/main/java/db_design_tool/app/home/HomeController.java
    登録済みテーブル名の一覧を画面へ送信する処理を追加。

--- src/main/java/db_design_tool/app/home/HomeController.java
+++ src/main/java/db_design_tool/app/home/HomeController.java
@@ -1,6 +1,7 @@
 package db_design_tool.app.home;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -10,31 +11,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import db_design_tool.domain.model.TableMaster;
+import db_design_tool.domain.service.table_definition.TableDefinitionService;
+import db_design_tool.domain.service.table_definition.TableDefinitionServiceImpl;
+
 /**
- * Servlet implementation class HomeController
+ * ホーム画面を管理する。
  */
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private final TableDefinitionService service;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
+    public HomeController() throws Exception {
         super();
+        service = new TableDefinitionServiceImpl();
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * ホーム画面へ登録済みのテーブル名とクエリ名の一覧を送信する。
+     *
+     * @param request
+     *            {@link javax.servlet.http.HttpServletRequest}
+     * @param response
+     *            {@link javax.servlet.http.HttpServletResponse}
+     * @throws ServletException
+     *             {@link javax.servlet.ServletException}
+     * @throws IOException
+     *             {@link java.io.IOException}
      */
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        final ServletContext context = getServletContext();
-        final RequestDispatcher dispatcher = context
-                .getRequestDispatcher("/WEB-INF/home/index.jsp");
-        dispatcher.include(request, response);
-    }
+        try {
+            final List<TableMaster> tableMasterList = service.findTableAll();
+            request.setAttribute("tableMasterList", tableMasterList);
 
+            final ServletContext context = getServletContext();
+            final RequestDispatcher dispatcher = context
+                    .getRequestDispatcher("/WEB-INF/home/index.jsp");
+            dispatcher.include(request, response);
+        } catch (final Exception e) {
+            throw new IOException(e);
+        }
+    }
 }
