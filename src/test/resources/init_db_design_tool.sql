0962267a1175c4f5e31e56f018edfcad993a040a
JhonnyBravo <sanfranceshika5@gmail.com>
2020-04-25 02:04:46 +0900

    modified: src/test/resources/init_db_design_tool.sql
    new tables: table_source_definition, field_source_definition

--- src/test/resources/init_db_design_tool.sql
+++ src/test/resources/init_db_design_tool.sql
@@ -26,4 +26,29 @@ CREATE TABLE IF NOT EXISTS field_master (
         ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS table_source_definition (
+    table_id INT NOT NULL,
+    definition_id INT AUTO_INCREMENT,
+    source_id INT NOT NULL,
+    no INT NOT NULL,
+    join_condition VARCHAR(255),
+    delete_flag INT NOT NULL,
+    PRIMARY KEY (definition_id),
+    INDEX (table_id),
+    FOREIGN KEY (table_id)
+        REFERENCES table_master (table_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS field_source_definition (
+    field_id INT NOT NULL,
+    definition_id INT AUTO_INCREMENT,
+    source_definition VARCHAR(255),
+    PRIMARY KEY (definition_id),
+    INDEX (field_id),
+    FOREIGN KEY (field_id)
+        REFERENCES field_master (field_id)
+        ON DELETE CASCADE
+);
+
 -- grant all privileges on db_design_tool.* to user@localhost;
