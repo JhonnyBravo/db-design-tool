804743e6e7f4feb8c104c553ec8c5560bdb75a17
JhonnyBravo <sanfranceshika5@gmail.com>
2020-06-21 09:55:45 +0900

    new file: WebContent/resources/js/webpack.config.js

---
+++ WebContent/resources/js/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require( "path" );
+
+module.exports = {
+    mode: "development",
+    entry: {
+        "dist/table_definition": "./src/table_definition/index.js",
+        "dist/select_definition": "./src/select_definition/index.js"
+    },
+    output: {
+        path: __dirname,
+        filename: "[name].bundle.js"
+    },
+    resolve: {
+        modules: [
+            path.join( __dirname, "src" ),
+            "node_modules"
+        ],
+        extensions: [ ".js" ],
+    },
+    module: {
+        rules: [
+            {
+                test: "/\.js$/",
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
