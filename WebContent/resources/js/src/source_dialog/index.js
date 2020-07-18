1b154fadc305c7ff64e40689985473d32a32d6f5
JhonnyBravo <sanfranceshika5@gmail.com>
2020-07-19 00:32:56 +0900

    modified: WebContent/resources/js/src/source_dialog/index.js
    $.data() を導入。

--- WebContent/resources/js/src/source_dialog/index.js
+++ WebContent/resources/js/src/source_dialog/index.js
@@ -3,14 +3,30 @@ import $ from "jquery";
 export default {
     /**
      * 結合条件入力ダイアログの画面表示を初期化する。
-     * @param definition ダイアログへ渡す初期値を設定したオブジェクトを指定する。
+     */
+    init () {
+        $( this ).data( "tableSourceDefinition", {
+            no: 0,
+            tableId: $( this ).find( "select[name='tableName'] option:first-child" ).val(),
+            tableName: null,
+            condition: null
+        } );
+
+        var definition = $( this ).data( "tableSourceDefinition" );
+        $( this ).find( "input[name='no']" ).val( definition.no );
+        $( this ).find( "select[name='tableName']" ).val( definition.tableId );
+        $( this ).find( "textarea[name='condition']" ).val( definition.condition );
+    },
+    /**
+     *結合条件入力ダイアログへ取得元テーブルの定義を登録する。
+     * @param definition
      * {
      *  no: no,
      *  tableId: 取得元エンティティの table_id,
      *  condition: 結合条件
      * }
      */
-    init ( definition ) {
+    setDefinition ( definition ) {
         $( this ).find( "input[name='no']" ).val( definition.no );
         $( this ).find( "select[name='tableName']" ).val( definition.tableId );
         $( this ).find( "textarea[name='condition']" ).val( definition.condition );
@@ -25,33 +41,13 @@ export default {
      * }
      */
     getDefinition () {
-        var definition = {};
+        $( this ).data( "tableSourceDefinition", {
+            no: $( this ).find( "input[name='no']" ).val(),
+            tableId: $( this ).find( "select[name='tableName']" ).val(),
+            tableName: $( this ).find( "select[name='tableName']" ).find( "option:selected" ).text(),
+            condition: $( this ).find( "textarea[name='condition']" ).val()
+        } );
 
-        definition.no = $( this ).find( "input[name='no']" ).val();
-        definition.tableId = $( this ).find( "select[name='tableName']" ).val();
-        definition.tableName = $( this ).find( "select[name='tableName']" )
-            .find( "option:selected" ).text();
-        definition.condition = $( this ).find( "textarea[name='condition']" )
-            .val();
-
-        return definition;
-    },
-    /**
-     * @returns definition 結合条件入力ダイアログのデフォルト設定値を設定したオブジェクトを返す。
-     * {
-     *  no: no,
-     *  tableId: 取得元エンティティの table_id,
-     *  condition: 結合条件
-     * }
-     */
-    createDefinition () {
-        var definition = {};
-
-        definition.no = 0;
-        definition.tableId = $( this ).find(
-            "select[name='tableName'] option:first-child" ).val();
-        definition.condition = null;
-
-        return definition;
+        return $( this ).data( "tableSourceDefinition" );
     }
 };
