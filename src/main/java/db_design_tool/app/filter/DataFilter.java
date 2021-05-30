c3957a350d4d1c907bfc46eb0a70e2028894147e
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:47:05 +0900

    modified: src/main/java/db_design_tool/app/filter/DataFilter.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/app/filter/DataFilter.java
+++ src/main/java/db_design_tool/app/filter/DataFilter.java
@@ -1,7 +1,6 @@
 package db_design_tool.app.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -11,32 +10,21 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 /**
- * Servlet Filter implementation class DataFilter
+ * JSON データ送信時の文字コードとコンテンツタイプを設定する。
  */
 @WebFilter("/data/*")
 public class DataFilter implements Filter {
-    /**
-     * @see Filter#init(FilterConfig)
-     */
-    @Override
-    public void init(FilterConfig fConfig) throws ServletException {
-    }
+  @Override
+  public void init(FilterConfig fConfig) throws ServletException {}
 
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json; charset=UTF-8");
-        chain.doFilter(request, response);
-    }
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
+    response.setContentType("application/json; charset=UTF-8");
+    chain.doFilter(request, response);
+  }
 
-    /**
-     * @see Filter#destroy()
-     */
-    @Override
-    public void destroy() {
-    }
+  @Override
+  public void destroy() {}
 }
