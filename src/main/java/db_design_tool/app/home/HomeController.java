f61e2c6db75c5dda9afc484fa4bc065a3eb88047
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:48:36 +0900

    modified: src/main/java/db_design_tool/app/home/HomeController.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/app/home/HomeController.java
+++ src/main/java/db_design_tool/app/home/HomeController.java
@@ -1,8 +1,10 @@
 package db_design_tool.app.home;
 
+import db_design_tool.domain.model.TableMaster;
+import db_design_tool.domain.service.table_definition.TableDefinitionService;
+import db_design_tool.domain.service.table_definition.TableDefinitionServiceImpl;
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,53 +13,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import db_design_tool.domain.model.TableMaster;
-import db_design_tool.domain.service.table_definition.TableDefinitionService;
-import db_design_tool.domain.service.table_definition.TableDefinitionServiceImpl;
-
 /**
  * ホーム画面を管理する。
  */
 @WebServlet("/definition")
 public class HomeController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private final TableDefinitionService service;
+  private static final long serialVersionUID = 1L;
+  private final TableDefinitionService service;
 
-    public HomeController() throws Exception {
-        super();
-        service = new TableDefinitionServiceImpl();
+  public HomeController() throws Exception {
+    super();
+    service = new TableDefinitionServiceImpl();
+  }
+
+  /**
+   * ホーム画面へ登録済みのテーブル名とクエリ名の一覧を送信する。
+   *
+   * @param request {@link javax.servlet.http.HttpServletRequest}
+   * @param response {@link javax.servlet.http.HttpServletResponse}
+   * @throws ServletException {@link javax.servlet.ServletException}
+   * @throws IOException {@link java.io.IOException}
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    try {
+      final List<TableMaster> tableList = service.findTableByEntityType(1);
+      request.setAttribute("tableList", tableList);
+
+      final List<TableMaster> queryList = service.findTableByEntityType(2);
+      request.setAttribute("queryList", queryList);
+
+      final ServletContext context = getServletContext();
+      final RequestDispatcher dispatcher = context.getRequestDispatcher("/WEB-INF/home/index.jsp");
+      dispatcher.include(request, response);
+    } catch (final Exception e) {
+      throw new IOException(e);
     }
-
-    /**
-     * ホーム画面へ登録済みのテーブル名とクエリ名の一覧を送信する。
-     *
-     * @param request
-     *            {@link javax.servlet.http.HttpServletRequest}
-     * @param response
-     *            {@link javax.servlet.http.HttpServletResponse}
-     * @throws ServletException
-     *             {@link javax.servlet.ServletException}
-     * @throws IOException
-     *             {@link java.io.IOException}
-     */
-    @Override
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        try {
-            final List<TableMaster> tableList = service
-                    .findTableByEntityType(1);
-            request.setAttribute("tableList", tableList);
-
-            final List<TableMaster> queryList = service
-                    .findTableByEntityType(2);
-            request.setAttribute("queryList", queryList);
-
-            final ServletContext context = getServletContext();
-            final RequestDispatcher dispatcher = context
-                    .getRequestDispatcher("/WEB-INF/home/index.jsp");
-            dispatcher.include(request, response);
-        } catch (final Exception e) {
-            throw new IOException(e);
-        }
-    }
+  }
 }
