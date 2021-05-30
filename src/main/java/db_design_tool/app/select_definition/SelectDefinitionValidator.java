ab64927d06f2616f4c98f0f396eb0063978b7551
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:43:11 +0900

    modified: src/main/java/db_design_tool/app/select_definition/SelectDefinitionValidator.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/app/select_definition/SelectDefinitionValidator.java
+++ src/main/java/db_design_tool/app/select_definition/SelectDefinitionValidator.java
@@ -1,80 +1,82 @@
 package db_design_tool.app.select_definition;
 
-import java.util.Set;
-
 import db_design_tool.app.table_definition.TableDefinitionValidator;
 import db_design_tool.domain.model.FieldSourceDefinition;
 import db_design_tool.domain.model.TableSourceDefinition;
 import jakarta.validation.ConstraintViolation;
+import java.util.Set;
 
+/**
+ * クエリ定義編集画面から送信されたデータのバリデーションチェックを管理する。
+ */
 public class SelectDefinitionValidator extends TableDefinitionValidator {
-    /**
-     * TableSourceDefinition のバリデーションチェックを実行する。
-     *
-     * @param tableSourceDefinition チェック対象とする TableSourceDefinition を指定する。
-     * @return hasError エラーの有無を真偽値で返す。
-     *         <ul>
-     *         <li>true: エラーが有ることを表す。</li>
-     *         <li>false: エラーが無いことを表す。</li>
-     *         </ul>
-     */
-    public boolean validateTableSourceDefinition(TableSourceDefinition tableSourceDefinition) {
-        boolean hasError = false;
-        final Set<ConstraintViolation<TableSourceDefinition>> tableSourceDefinitionViolation = validator
-                .validate(tableSourceDefinition);
+  /**
+   * TableSourceDefinition のバリデーションチェックを実行する。
+   *
+   * @param tableSourceDefinition チェック対象とする TableSourceDefinition を指定する。
+   * @return hasError エラーの有無を真偽値で返す。
+   *         <ul>
+   *         <li>true: エラーが有ることを表す。</li>
+   *         <li>false: エラーが無いことを表す。</li>
+   *         </ul>
+   */
+  public boolean validateTableSourceDefinition(TableSourceDefinition tableSourceDefinition) {
+    boolean hasError = false;
+    final Set<ConstraintViolation<TableSourceDefinition>> tableSourceDefinitionViolation =
+        validator.validate(tableSourceDefinition);
 
-        if (tableSourceDefinitionViolation.size() > 0) {
-            hasError = true;
+    if (tableSourceDefinitionViolation.size() > 0) {
+      hasError = true;
 
-            tableSourceDefinitionViolation.stream().forEach(e -> {
-                final String path = e.getPropertyPath().toString();
+      tableSourceDefinitionViolation.stream().forEach(e -> {
+        final String path = e.getPropertyPath().toString();
 
-                if (path.equals("joinCondition")) {
-                    tableSourceDefinition.setJoinConditionError(e.getMessage());
-                }
-            });
+        if ("joinCondition".equals(path)) {
+          tableSourceDefinition.setJoinConditionError(e.getMessage());
         }
-
-        return hasError;
+      });
     }
 
-    /**
-     * FieldMaster のバリデーションチェックを実行する。
-     *
-     * @param fieldSourceDefinition チェック対象とする FieldMaster を指定する。
-     * @return hasError エラーの有無を真偽値で返す。
-     *         <ul>
-     *         <li>true: エラーが有ることを表す。</li>
-     *         <li>false: エラーが無いことを表す。</li>
-     *         </ul>
-     */
-    public boolean validateFieldSourceDefinition(FieldSourceDefinition fieldSourceDefinition) {
-        boolean hasError = false;
-        final Set<ConstraintViolation<FieldSourceDefinition>> fieldSourceDefinitionViolation = validator
-                .validate(fieldSourceDefinition);
+    return hasError;
+  }
 
-        if (fieldSourceDefinitionViolation.size() > 0) {
-            hasError = true;
+  /**
+   * FieldMaster のバリデーションチェックを実行する。
+   *
+   * @param fieldSourceDefinition チェック対象とする FieldMaster を指定する。
+   * @return hasError エラーの有無を真偽値で返す。
+   *         <ul>
+   *         <li>true: エラーが有ることを表す。</li>
+   *         <li>false: エラーが無いことを表す。</li>
+   *         </ul>
+   */
+  public boolean validateFieldSourceDefinition(FieldSourceDefinition fieldSourceDefinition) {
+    boolean hasError = false;
+    final Set<ConstraintViolation<FieldSourceDefinition>> fieldSourceDefinitionViolation =
+        validator.validate(fieldSourceDefinition);
 
-            fieldSourceDefinitionViolation.stream().forEach(e -> {
-                final String path = e.getPropertyPath().toString();
+    if (fieldSourceDefinitionViolation.size() > 0) {
+      hasError = true;
 
-                if (path.equals("physicalName")) {
-                    fieldSourceDefinition.setPhysicalNameError(e.getMessage());
-                } else if (path.equals("logicalName")) {
-                    fieldSourceDefinition.setLogicalNameError(e.getMessage());
-                } else if (path.equals("description")) {
-                    fieldSourceDefinition.setDescriptionError(e.getMessage());
-                } else if (path.equals("sourceDefinition")) {
-                    fieldSourceDefinition.setSourceDefinitionError(e.getMessage());
-                }
-            });
+      fieldSourceDefinitionViolation.stream().forEach(e -> {
+        final String path = e.getPropertyPath().toString();
 
-            if (fieldSourceDefinition.getDataSizeError() != null) {
-                hasError = true;
-            }
+        if ("physicalName".equals(path)) {
+          fieldSourceDefinition.setPhysicalNameError(e.getMessage());
+        } else if ("logicalName".equals(path)) {
+          fieldSourceDefinition.setLogicalNameError(e.getMessage());
+        } else if ("description".equals(path)) {
+          fieldSourceDefinition.setDescriptionError(e.getMessage());
+        } else if ("sourceDefinition".equals(path)) {
+          fieldSourceDefinition.setSourceDefinitionError(e.getMessage());
         }
+      });
 
-        return hasError;
+      if (fieldSourceDefinition.getDataSizeError() != null) {
+        hasError = true;
+      }
     }
+
+    return hasError;
+  }
 }
