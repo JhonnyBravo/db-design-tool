f841e33b0eece6e33555e91f0498769ee0b4e97d
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:39:46 +0900

    modified: src/main/java/db_design_tool/domain/service/table_definition/TableDefinitionServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/service/table_definition/TableDefinitionServiceImpl.java
+++ src/main/java/db_design_tool/domain/service/table_definition/TableDefinitionServiceImpl.java
@@ -1,174 +1,156 @@
 package db_design_tool.domain.service.table_definition;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import db_design_tool.domain.model.FieldMaster;
 import db_design_tool.domain.model.TableDefinition;
 import db_design_tool.domain.model.TableMaster;
 import db_design_tool.domain.repository.field_master.FieldMasterRepository;
 import db_design_tool.domain.repository.table_master.TableMasterRepository;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class TableDefinitionServiceImpl implements TableDefinitionService {
-    private final SqlSessionFactory factory;
-    private TableMasterRepository tableMasterRepository;
-    private FieldMasterRepository fieldMasterRepository;
+  private final SqlSessionFactory factory;
+  private TableMasterRepository tableMasterRepository;
+  private FieldMasterRepository fieldMasterRepository;
 
-    public TableDefinitionServiceImpl() throws Exception {
-        factory = new SqlSessionFactoryBuilder()
-                .build(Resources.getResourceAsStream("mybatis-config.xml"));
+  public TableDefinitionServiceImpl() throws Exception {
+    factory =
+        new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+  }
+
+  @Override
+  public List<TableMaster> findTableAll() throws Exception {
+    List<TableMaster> recordset = new ArrayList<>();
+
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      recordset = tableMasterRepository.findAll();
     }
 
-    @Override
-    public List<TableMaster> findTableAll() throws Exception {
-        List<TableMaster> recordset = new ArrayList<>();
+    return recordset;
+  }
 
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            recordset = tableMasterRepository.findAll();
-        }
+  @Override
+  public FieldMaster findFieldByFieldId(int fieldId) throws Exception {
+    FieldMaster record = new FieldMaster();
 
-        return recordset;
+    try (SqlSession session = factory.openSession()) {
+      fieldMasterRepository = session.getMapper(FieldMasterRepository.class);
+      record = fieldMasterRepository.findByFieldId(fieldId);
     }
 
-    @Override
-    public FieldMaster findFieldByFieldId(int fieldId) throws Exception {
-        FieldMaster record = new FieldMaster();
+    return record;
+  }
 
-        try (SqlSession session = factory.openSession()) {
-            fieldMasterRepository = session
-                    .getMapper(FieldMasterRepository.class);
-            record = fieldMasterRepository.findByFieldId(fieldId);
-        }
+  @Override
+  public List<TableMaster> findTableByEntityType(int entityType) throws Exception {
+    List<TableMaster> recordset = new ArrayList<>();
 
-        return record;
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      recordset = tableMasterRepository.findByEntityType(entityType);
     }
 
-    @Override
-    public List<TableMaster> findTableByEntityType(int entityType)
-            throws Exception {
-        List<TableMaster> recordset = new ArrayList<>();
+    return recordset;
+  }
 
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            recordset = tableMasterRepository.findByEntityType(entityType);
-        }
+  @Override
+  public TableDefinition findTableDefinitionByTableId(int tableId) throws Exception {
+    TableMaster tableMaster = new TableMaster();
+    List<FieldMaster> fieldMasterList = new ArrayList<>();
+    final TableDefinition tableDefinition = new TableDefinition();
 
-        return recordset;
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMaster = tableMasterRepository.findByTableId(tableId);
+
+      fieldMasterRepository = session.getMapper(FieldMasterRepository.class);
+      fieldMasterList = fieldMasterRepository.findByTableId(tableId);
     }
 
-    @Override
-    public TableDefinition findTableDefinitionByTableId(int tableId)
-            throws Exception {
-        TableMaster tableMaster = new TableMaster();
-        List<FieldMaster> fieldMasterList = new ArrayList<>();
-        final TableDefinition tableDefinition = new TableDefinition();
+    tableDefinition.setTableMaster(tableMaster);
+    tableDefinition
+        .setFieldMaster(fieldMasterList.toArray(new FieldMaster[fieldMasterList.size()]));
 
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMaster = tableMasterRepository.findByTableId(tableId);
+    return tableDefinition;
+  }
 
-            fieldMasterRepository = session
-                    .getMapper(FieldMasterRepository.class);
-            fieldMasterList = fieldMasterRepository.findByTableId(tableId);
-        }
+  @Override
+  public void create(TableDefinition tableDefinition) throws Exception {
+    final TableMaster tableMaster = tableDefinition.getTableMaster();
+    final FieldMaster[] fieldMasterArray = tableDefinition.getFieldMaster();
 
-        tableDefinition.setTableMaster(tableMaster);
-        tableDefinition.setFieldMaster(fieldMasterList
-                .toArray(new FieldMaster[fieldMasterList.size()]));
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMaster.setEntityType(1);
+      tableMasterRepository.create(tableMaster);
 
-        return tableDefinition;
+      final TableMaster latest = tableMasterRepository.findByLatestTableId();
+
+      for (final FieldMaster fieldMaster : fieldMasterArray) {
+        fieldMaster.setTableId(latest.getTableId());
+      }
+
+      fieldMasterRepository = session.getMapper(FieldMasterRepository.class);
+      fieldMasterRepository.create(Arrays.asList(fieldMasterArray));
+
+      session.commit();
     }
+  }
 
-    @Override
-    public void create(TableDefinition tableDefinition) throws Exception {
-        final TableMaster tableMaster = tableDefinition.getTableMaster();
-        final FieldMaster[] fieldMasterArray = tableDefinition.getFieldMaster();
+  @Override
+  public void update(TableDefinition tableDefinition) throws Exception {
+    final TableMaster tableMaster = tableDefinition.getTableMaster();
+    final FieldMaster[] fieldMasterArray = tableDefinition.getFieldMaster();
 
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMaster.setEntityType(1);
-            tableMasterRepository.create(tableMaster);
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMasterRepository.update(tableMaster);
 
-            final TableMaster latest = tableMasterRepository
-                    .findByLatestTableId();
+      fieldMasterRepository = session.getMapper(FieldMasterRepository.class);
+      fieldMasterRepository.update(Arrays.asList(fieldMasterArray));
+      fieldMasterRepository.deleteByDeleteFlag();
 
-            for (final FieldMaster fieldMaster : fieldMasterArray) {
-                fieldMaster.setTableId(latest.getTableId());
-            }
-
-            fieldMasterRepository = session
-                    .getMapper(FieldMasterRepository.class);
-            fieldMasterRepository.create(Arrays.asList(fieldMasterArray));
-
-            session.commit();
-        }
+      session.commit();
     }
+  }
 
-    @Override
-    public void update(TableDefinition tableDefinition) throws Exception {
-        final TableMaster tableMaster = tableDefinition.getTableMaster();
-        final FieldMaster[] fieldMasterArray = tableDefinition.getFieldMaster();
-
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMasterRepository.update(tableMaster);
-
-            fieldMasterRepository = session
-                    .getMapper(FieldMasterRepository.class);
-            fieldMasterRepository.update(Arrays.asList(fieldMasterArray));
-            fieldMasterRepository.deleteByDeleteFlag();
-
-            session.commit();
-        }
+  @Override
+  public void deleteByTableId(int tableId) throws Exception {
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMasterRepository.deleteByTableId(tableId);
+      session.commit();
     }
+  }
 
-    @Override
-    public void deleteByTableId(int tableId) throws Exception {
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMasterRepository.deleteByTableId(tableId);
-            session.commit();
-        }
+  @Override
+  public void deleteByEntityType(int entityType) throws Exception {
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMasterRepository.deleteByEntityType(entityType);
+      session.commit();
     }
+  }
 
-    @Override
-    public void deleteByEntityType(int entityType) throws Exception {
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMasterRepository.deleteByEntityType(entityType);
-            session.commit();
-        }
+  @Override
+  public void deleteAll() throws Exception {
+    try (SqlSession session = factory.openSession()) {
+      tableMasterRepository = session.getMapper(TableMasterRepository.class);
+      tableMasterRepository.deleteAll();
+      tableMasterRepository.resetId();
+
+      fieldMasterRepository = session.getMapper(FieldMasterRepository.class);
+      fieldMasterRepository.deleteAll();
+      fieldMasterRepository.resetId();
+
+      session.commit();
     }
-
-    @Override
-    public void deleteAll() throws Exception {
-        try (SqlSession session = factory.openSession()) {
-            tableMasterRepository = session
-                    .getMapper(TableMasterRepository.class);
-            tableMasterRepository.deleteAll();
-            tableMasterRepository.resetId();
-
-            fieldMasterRepository = session
-                    .getMapper(FieldMasterRepository.class);
-            fieldMasterRepository.deleteAll();
-            fieldMasterRepository.resetId();
-
-            session.commit();
-        }
-    }
+  }
 
 }
