2fc007af3c82e9e509a9f475d122d88c37096df1
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:26:21 +0900

    modified: src/main/java/db_design_tool/domain/model/TableDefinition.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/model/TableDefinition.java
+++ src/main/java/db_design_tool/domain/model/TableDefinition.java
@@ -2,39 +2,48 @@ package db_design_tool.domain.model;
 
 import java.io.Serializable;
 
+/**
+ * table_definition のエンティティ。
+ */
 public class TableDefinition implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private TableMaster tableMaster;
-    private FieldMaster[] fieldMaster;
+  private TableMaster tableMaster;
+  private FieldMaster[] fieldMaster;
 
-    /**
-     * @return tableMaster TableMaster を返す。
-     */
-    public TableMaster getTableMaster() {
-        return tableMaster;
-    }
+  /**
+   * {@link TableMaster} を返す。
+   *
+   * @return tableMaster {@link TableMaster}
+   */
+  public TableMaster getTableMaster() {
+    return tableMaster;
+  }
 
-    /**
-     * @param tableMaster
-     *            TableMaster を指定する。
-     */
-    public void setTableMaster(TableMaster tableMaster) {
-        this.tableMaster = tableMaster;
-    }
+  /**
+   * {@link TableMaster} を設定する。
+   *
+   * @param tableMaster {@link TableMaster}
+   */
+  public void setTableMaster(TableMaster tableMaster) {
+    this.tableMaster = tableMaster;
+  }
 
-    /**
-     * @return fieldMaster FieldMaster の配列を返す。
-     */
-    public FieldMaster[] getFieldMaster() {
-        return fieldMaster;
-    }
+  /**
+   * {@link FieldMaster} の配列を返す。
+   *
+   * @return fieldMaster {@link FieldMaster} の配列
+   */
+  public FieldMaster[] getFieldMaster() {
+    return fieldMaster;
+  }
 
-    /**
-     * @param fieldMaster
-     *            FieldMaster の配列を指定する。
-     */
-    public void setFieldMaster(FieldMaster[] fieldMaster) {
-        this.fieldMaster = fieldMaster;
-    }
+  /**
+   * {@link FieldMaster} を設定する。
+   *
+   * @param fieldMaster {@link FieldMaster} の配列
+   */
+  public void setFieldMaster(FieldMaster[] fieldMaster) {
+    this.fieldMaster = fieldMaster;
+  }
 }
