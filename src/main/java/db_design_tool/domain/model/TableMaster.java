69600328fa343d12b861224d543de062e36f8ccf
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:27:08 +0900

    modified: src/main/java/db_design_tool/domain/model/TableMaster.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/model/TableMaster.java
+++ src/main/java/db_design_tool/domain/model/TableMaster.java
@@ -1,128 +1,157 @@
 package db_design_tool.domain.model;
 
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
-
 import org.hibernate.validator.constraints.Range;
 
-import jakarta.validation.constraints.Size;
-
+/**
+ * table_master のエンティティ。
+ */
 public class TableMaster implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private int tableId;
-    @Size(min = 1, max = 100)
-    private String physicalName;
-    @Size(min = 1, max = 100)
-    private String logicalName;
-    @Range(min = 0, max = 1)
-    private int deleteFlag;
-    private int entityType;
-    private String physicalNameError;
-    private String logicalNameError;
+  private int tableId;
+  @Size(min = 1, max = 100)
+  private String physicalName;
+  @Size(min = 1, max = 100)
+  private String logicalName;
+  @Range(min = 0, max = 1)
+  private int deleteFlag;
+  private int entityType;
+  private String physicalNameError;
+  private String logicalNameError;
 
-    /**
-     * @return tableId テーブルの ID を返す。
-     */
-    public int getTableId() {
-        return tableId;
-    }
+  /**
+   * テーブル ID を返す。
+   *
+   * @return tableId テーブルの ID
+   */
+  public int getTableId() {
+    return tableId;
+  }
 
-    /**
-     * @param tableId テーブルの ID を指定する。
-     */
-    public void setTableId(int tableId) {
-        this.tableId = tableId;
-    }
+  /**
+   * テーブル ID を設定する。
+   *
+   * @param tableId テーブルの ID として設定する数値を指定する。
+   */
+  public void setTableId(int tableId) {
+    this.tableId = tableId;
+  }
 
-    /**
-     * @return physicalName テーブルの物理名を返す。
-     */
-    public String getPhysicalName() {
-        return physicalName;
-    }
+  /**
+   * テーブルの物理名を返す。
+   *
+   * @return physicalName テーブルの物理名
+   */
+  public String getPhysicalName() {
+    return physicalName;
+  }
 
-    /**
-     * @param physicalName テーブルの物理名を指定する。
-     */
-    public void setPhysicalName(String physicalName) {
-        this.physicalName = physicalName;
-    }
+  /**
+   * テーブルの物理名を設定する。
+   *
+   * @param physicalName テーブルの物理名として設定する文字列を指定する。
+   */
+  public void setPhysicalName(String physicalName) {
+    this.physicalName = physicalName;
+  }
 
-    /**
-     * @return logicalName テーブルの論理名を返す。
-     */
-    public String getLogicalName() {
-        return logicalName;
-    }
+  /**
+   * テーブルの論理名を返す。
+   *
+   * @return logicalName テーブルの論理名
+   */
+  public String getLogicalName() {
+    return logicalName;
+  }
 
-    /**
-     * @param logicalName テーブルの論理名を指定する。
-     */
-    public void setLogicalName(String logicalName) {
-        this.logicalName = logicalName;
-    }
+  /**
+   * テーブルの論理名を設定する。
+   *
+   * @param logicalName テーブルの論理名として設定する文字列を指定する。
+   */
+  public void setLogicalName(String logicalName) {
+    this.logicalName = logicalName;
+  }
 
-    /**
-     * @return deleteFlag 削除フラグを返す。
-     */
-    public int getDeleteFlag() {
-        return deleteFlag;
-    }
+  /**
+   * 削除フラグを返す。
+   *
+   * @return deleteFlag 削除フラグ
+   */
+  public int getDeleteFlag() {
+    return deleteFlag;
+  }
 
-    /**
-     * @param deleteFlag 削除フラグを指定する。
-     *                   <ul>
-     *                   <li>0: 削除されていないことを表す。</li>
-     *                   <li>1: 削除されていることを表す。</li>
-     *                   </ul>
-     */
-    public void setDeleteFlag(int deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
+  /**
+   * 削除フラグを設定する。
+   *
+   * @param deleteFlag 削除フラグとして設定する数値を指定する。
+   *        <ul>
+   *        <li>0: 削除されていないことを表す。</li>
+   *        <li>1: 削除されていることを表す。</li>
+   *        </ul>
+   */
+  public void setDeleteFlag(int deleteFlag) {
+    this.deleteFlag = deleteFlag;
+  }
 
-    /**
-     * @return entityType エンティティタイプを返す。
-     */
-    public int getEntityType() {
-        return entityType;
-    }
+  /**
+   * エンティティタイプを返す。
+   *
+   * @return entityType エンティティタイプ
+   */
+  public int getEntityType() {
+    return entityType;
+  }
 
-    /**
-     * @param entityType エンティティタイプを指定する。
-     *                   <ul>
-     *                   <li>1: テーブル</li>
-     *                   <li>2: クエリ</li>
-     *                   </ul>
-     */
-    public void setEntityType(int entityType) {
-        this.entityType = entityType;
-    }
+  /**
+   * エンティティタイプを設定する。
+   *
+   * @param entityType エンティティタイプとして設定する数値を指定する。
+   *        <ul>
+   *        <li>1: テーブル</li>
+   *        <li>2: クエリ</li>
+   *        </ul>
+   */
+  public void setEntityType(int entityType) {
+    this.entityType = entityType;
+  }
 
-    /**
-     * @return physicalNameError テーブルの物理名に関するエラーメッセージを返す。
-     */
-    public String getPhysicalNameError() {
-        return physicalNameError;
-    }
+  /**
+   * テーブルの物理名に関するエラーメッセージを返す。
+   *
+   * @return physicalNameError テーブルの物理名に関するエラーメッセージ
+   */
+  public String getPhysicalNameError() {
+    return physicalNameError;
+  }
 
-    /**
-     * @param physicalNameError テーブルの物理名に関するエラーメッセージを指定する。
-     */
-    public void setPhysicalNameError(String physicalNameError) {
-        this.physicalNameError = physicalNameError;
-    }
+  /**
+   * テーブルの物理名に関するエラーメッセージを設定する。
+   *
+   * @param physicalNameError エラーメッセージとして設定する文字列を指定する。
+   */
+  public void setPhysicalNameError(String physicalNameError) {
+    this.physicalNameError = physicalNameError;
+  }
 
-    /**
-     * @return logicalNameError テーブルの論理名に関するエラーメッセージを返す。
-     */
-    public String getLogicalNameError() {
-        return logicalNameError;
-    }
+  /**
+   * テーブルの論理名に関するエラーメッセージを返す。
+   *
+   * @return logicalNameError テーブルの論理名に関するエラーメッセージ
+   */
+  public String getLogicalNameError() {
+    return logicalNameError;
+  }
 
-    /**
-     * @param logicalNameError テーブルの論理名に関するエラーメッセージを指定する。
-     */
-    public void setLogicalNameError(String logicalNameError) {
-        this.logicalNameError = logicalNameError;
-    }
+  /**
+   * テーブルの論理名に関するエラーメッセージを設定する。
+   *
+   * @param logicalNameError エラーメッセージとして設定する文字列を指定する。
+   */
+  public void setLogicalNameError(String logicalNameError) {
+    this.logicalNameError = logicalNameError;
+  }
 }
