975bbeabcd891186b73889a17f7432eaf9c3f721
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-30 23:24:31 +0900

    modified: src/main/java/db_design_tool/domain/model/FieldSourceDefinition.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/model/FieldSourceDefinition.java
+++ src/main/java/db_design_tool/domain/model/FieldSourceDefinition.java
@@ -1,56 +1,70 @@
 package db_design_tool.domain.model;
 
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 
-import jakarta.validation.constraints.Size;
-
+/**
+ * field_source_definition のエンティティ。
+ */
 public class FieldSourceDefinition extends FieldMaster implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private int definitionId;
-    @Size(min = 1, max = 255)
-    private String sourceDefinition;
-    private String sourceDefinitionError;
+  private int definitionId;
+  @Size(min = 1, max = 255)
+  private String sourceDefinition;
+  private String sourceDefinitionError;
 
-    /**
-     * @return definitionId 定義の ID を返す。
-     */
-    public int getDefinitionId() {
-        return definitionId;
-    }
+  /**
+   * フィールド定義の ID を返す。
+   *
+   * @return definitionId フィールド定義の ID
+   */
+  public int getDefinitionId() {
+    return definitionId;
+  }
 
-    /**
-     * @param definitionId 定義の ID を指定する。
-     */
-    public void setDefinitionId(int definitionId) {
-        this.definitionId = definitionId;
-    }
+  /**
+   * フィールド定義の ID を設定する。
+   *
+   * @param definitionId フィールド定義の ID として設定する数値を指定する。
+   */
+  public void setDefinitionId(int definitionId) {
+    this.definitionId = definitionId;
+  }
 
-    /**
-     * @return sourceDefinition データ取得元の定義を返す。
-     */
-    public String getSourceDefinition() {
-        return sourceDefinition;
-    }
+  /**
+   * データ取得元の定義を返す。
+   *
+   * @return sourceDefinition データ取得元の定義
+   */
+  public String getSourceDefinition() {
+    return sourceDefinition;
+  }
 
-    /**
-     * @param sourceDefinition データ取得元の定義を指定する。
-     */
-    public void setSourceDefinition(String sourceDefinition) {
-        this.sourceDefinition = sourceDefinition;
-    }
+  /**
+   * データ取得元の定義を設定する。
+   *
+   * @param sourceDefinition データ取得元の定義として設定する文字列を指定する。
+   */
+  public void setSourceDefinition(String sourceDefinition) {
+    this.sourceDefinition = sourceDefinition;
+  }
 
-    /**
-     * @return sourceDefinitionError データ取得元の定義に関するエラーメッセージを返す。
-     */
-    public String getSourceDefinitionError() {
-        return sourceDefinitionError;
-    }
+  /**
+   * データ取得元の定義に関するエラーメッセージを返す。
+   *
+   * @return sourceDefinitionError データ取得元の定義に関するエラーメッセージ
+   */
+  public String getSourceDefinitionError() {
+    return sourceDefinitionError;
+  }
 
-    /**
-     * @param sourceDefinitionError データ取得元の定義に関するエラーメッセージを指定する。
-     */
-    public void setSourceDefinitionError(String sourceDefinitionError) {
-        this.sourceDefinitionError = sourceDefinitionError;
-    }
+  /**
+   * データ取得元の定義に関するエラーメッセージを設定する。
+   *
+   * @param sourceDefinitionError エラーメッセージとして設定する文字列を指定する。
+   */
+  public void setSourceDefinitionError(String sourceDefinitionError) {
+    this.sourceDefinitionError = sourceDefinitionError;
+  }
 }
