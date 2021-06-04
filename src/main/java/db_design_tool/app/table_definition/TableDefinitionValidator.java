84e76fc3cc5dee19a0c5e71ff5d4f55aa987781f
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-06-05 01:47:58 +0900

    modified: src/main/java/db_design_tool/app/table_definition/TableDefinitionValidator.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/db_design_tool/app/table_definition/TableDefinitionValidator.java
+++ src/main/java/db_design_tool/app/table_definition/TableDefinitionValidator.java
@@ -1,85 +1,92 @@
 package db_design_tool.app.table_definition;
 
-import java.util.Set;
-
 import db_design_tool.domain.model.FieldMaster;
 import db_design_tool.domain.model.TableMaster;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import java.util.Set;
 
+/**
+ * テーブル定義入力画面から送信されたデータのバリデーションチェックを管理する。
+ */
 public class TableDefinitionValidator {
-    protected final Validator validator;
+  protected final Validator validator;
 
-    public TableDefinitionValidator() {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
+  /**
+   * 初期化処理を実行する。
+   */
+  public TableDefinitionValidator() {
+    validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
 
-    /**
-     * TableMaster のバリデーションチェックを実行する。
-     *
-     * @param tableMaster チェック対象とする TableMaster を指定する。
-     * @return hasError エラーの有無を真偽値で返す。
-     *         <ul>
-     *         <li>true: エラーが有ることを表す。</li>
-     *         <li>false: エラーが無いことを表す。</li>
-     *         </ul>
-     */
-    public boolean validateTableMaster(TableMaster tableMaster) {
-        boolean hasError = false;
-        final Set<ConstraintViolation<TableMaster>> tableMasterViolation = validator.validate(tableMaster);
+  /**
+   * TableMaster のバリデーションチェックを実行する。
+   *
+   * @param tableMaster チェック対象とする TableMaster を指定する。
+   * @return hasError エラーの有無を真偽値で返す。
+   *         <ul>
+   *         <li>true: エラーが有ることを表す。</li>
+   *         <li>false: エラーが無いことを表す。</li>
+   *         </ul>
+   */
+  public boolean validateTableMaster(TableMaster tableMaster) {
+    boolean hasError = false;
+    final Set<ConstraintViolation<TableMaster>> tableMasterViolation =
+        validator.validate(tableMaster);
 
-        if (tableMasterViolation.size() > 0) {
-            hasError = true;
+    if (tableMasterViolation.size() > 0) {
+      hasError = true;
 
-            tableMasterViolation.stream().forEach(e -> {
-                final String path = e.getPropertyPath().toString();
+      tableMasterViolation.stream().forEach(e -> {
+        final String path = e.getPropertyPath().toString();
 
-                if (path.equals("physicalName")) {
-                    tableMaster.setPhysicalNameError(e.getMessage());
-                } else if (path.equals("logicalName")) {
-                    tableMaster.setLogicalNameError(e.getMessage());
-                }
-            });
+        if ("physicalName".equals(path)) {
+          tableMaster.setPhysicalNameError(e.getMessage());
+        } else if ("logicalName".equals(path)) {
+          tableMaster.setLogicalNameError(e.getMessage());
         }
-
-        return hasError;
+      });
     }
 
-    /**
-     * FieldMaster のバリデーションチェックを実行する。
-     *
-     * @param fieldMaster チェック対象とする FieldMaster を指定する。
-     * @return hasError エラーの有無を真偽値で返す。
-     *         <ul>
-     *         <li>true: エラーが有ることを表す。</li>
-     *         <li>false: エラーが無いことを表す。</li>
-     *         </ul>
-     */
-    public boolean validateFieldMaster(FieldMaster fieldMaster) {
-        boolean hasError = false;
-        final Set<ConstraintViolation<FieldMaster>> fieldMasterViolation = validator.validate(fieldMaster);
+    return hasError;
+  }
 
-        if (fieldMasterViolation.size() > 0) {
-            hasError = true;
+  /**
+   * FieldMaster のバリデーションチェックを実行する。
+   *
+   * @param fieldMaster チェック対象とする FieldMaster を指定する。
+   * @return hasError エラーの有無を真偽値で返す。
+   *         <ul>
+   *         <li>true: エラーが有ることを表す。</li>
+   *         <li>false: エラーが無いことを表す。</li>
+   *         </ul>
+   */
+  public boolean validateFieldMaster(FieldMaster fieldMaster) {
+    boolean hasError = false;
+    final Set<ConstraintViolation<FieldMaster>> fieldMasterViolation =
+        validator.validate(fieldMaster);
 
-            fieldMasterViolation.stream().forEach(e -> {
-                final String path = e.getPropertyPath().toString();
+    if (fieldMasterViolation.size() > 0) {
+      hasError = true;
 
-                if (path.equals("physicalName")) {
-                    fieldMaster.setPhysicalNameError(e.getMessage());
-                } else if (path.equals("logicalName")) {
-                    fieldMaster.setLogicalNameError(e.getMessage());
-                } else if (path.equals("description")) {
-                    fieldMaster.setDescriptionError(e.getMessage());
-                }
-            });
+      fieldMasterViolation.stream().forEach(e -> {
+        final String path = e.getPropertyPath().toString();
 
-            if (fieldMaster.getDataSizeError() != null) {
-                hasError = true;
-            }
+        if ("physicalName".equals(path)) {
+          fieldMaster.setPhysicalNameError(e.getMessage());
+        } else if ("logicalName".equals(path)) {
+          fieldMaster.setLogicalNameError(e.getMessage());
+        } else if ("description".equals(path)) {
+          fieldMaster.setDescriptionError(e.getMessage());
         }
+      });
 
-        return hasError;
+      if (fieldMaster.getDataSizeError() != null) {
+        hasError = true;
+      }
     }
+
+    return hasError;
+  }
 }
