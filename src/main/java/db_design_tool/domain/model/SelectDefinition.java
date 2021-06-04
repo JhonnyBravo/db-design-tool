39fe01c44effd18f7e535975b8988c327976876c
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-06-05 01:23:42 +0900

    modified: src/main/java/db_design_tool/domain/model/SelectDefinition.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/model/SelectDefinition.java
+++ src/main/java/db_design_tool/domain/model/SelectDefinition.java
@@ -1,40 +1,47 @@
 package db_design_tool.domain.model;
 
+/**
+ * select_definition のエンティティ。
+ */
 public class SelectDefinition extends TableDefinition {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private TableSourceDefinition[] tableSourceDefinition;
-    private FieldSourceDefinition[] fieldSourceDefinition;
+  private TableSourceDefinition[] tableSourceDefinition;
+  private FieldSourceDefinition[] fieldSourceDefinition;
 
-    /**
-     * @return tableSourceDefinition TableSourceDefinition を返す。
-     */
-    public TableSourceDefinition[] getTableSourceDefinition() {
-        return tableSourceDefinition;
-    }
+  /**
+   * {@link TableSourceDefinition} を返す。
+   *
+   * @return tableSourceDefinition {@link TableSourceDefinition}
+   */
+  public TableSourceDefinition[] getTableSourceDefinition() {
+    return tableSourceDefinition;
+  }
 
-    /**
-     * @param tableSourceDefinition
-     *            TableSourceDefinition を指定する。
-     */
-    public void setTableSourceDefinition(
-            TableSourceDefinition[] tableSourceDefinition) {
-        this.tableSourceDefinition = tableSourceDefinition;
-    }
+  /**
+   * {@link TableSourceDefinition} を設定する。
+   *
+   * @param tableSourceDefinition {@link TableSourceDefinition} を指定する。
+   */
+  public void setTableSourceDefinition(TableSourceDefinition[] tableSourceDefinition) {
+    this.tableSourceDefinition = tableSourceDefinition;
+  }
 
-    /**
-     * @return fieldSourceDefinition FieldSourceDefinition を返す。
-     */
-    public FieldSourceDefinition[] getFieldSourceDefinition() {
-        return fieldSourceDefinition;
-    }
+  /**
+   * {@link FieldSourceDefinition} を返す。
+   *
+   * @return fieldSourceDefinition {@link FieldSourceDefinition} を返す。
+   */
+  public FieldSourceDefinition[] getFieldSourceDefinition() {
+    return fieldSourceDefinition;
+  }
 
-    /**
-     * @param fieldSourceDefinition
-     *            FieldSourceDefinition を指定する。
-     */
-    public void setFieldSourceDefinition(
-            FieldSourceDefinition[] fieldSourceDefinition) {
-        this.fieldSourceDefinition = fieldSourceDefinition;
-    }
+  /**
+   * {@link FieldSourceDefinition} を設定する。
+   *
+   * @param fieldSourceDefinition {@link FieldSourceDefinition} を指定する。
+   */
+  public void setFieldSourceDefinition(FieldSourceDefinition[] fieldSourceDefinition) {
+    this.fieldSourceDefinition = fieldSourceDefinition;
+  }
 }
