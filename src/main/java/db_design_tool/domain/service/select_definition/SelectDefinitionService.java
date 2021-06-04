ed38400856969c454695a8fe821fa0b8257b019c
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-06-05 01:39:55 +0900

    modified: src/main/java/db_design_tool/domain/service/select_definition/SelectDefinitionService.java
    checkstyle の指摘事項を修正。

--- src/main/java/db_design_tool/domain/service/select_definition/SelectDefinitionService.java
+++ src/main/java/db_design_tool/domain/service/select_definition/SelectDefinitionService.java
@@ -1,75 +1,66 @@
 package db_design_tool.domain.service.select_definition;
 
-import java.util.List;
-
 import db_design_tool.domain.model.SelectDefinition;
 import db_design_tool.domain.model.TableMaster;
+import java.util.List;
 
 /**
  * 選択クエリの定義を管理する。
  */
 public interface SelectDefinitionService {
-    /**
-     * @return recordset TableMaster に登録されているテーブル・クエリを全て取得する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public List<TableMaster> findEntityAll() throws Exception;
+  /**
+   * {@link TableMaster} に登録されているテーブル・クエリを全て取得する。
+   *
+   * @return recordset {@link TableMaster} の {@link List}
+   * @throws Exception {@link java.lang.Exception}
+   */
+  List<TableMaster> findEntityAll() throws Exception;
 
-    /**
-     * @return recordset TableMaster に登録されている選択クエリを全て取得する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public List<TableMaster> findQueryAll() throws Exception;
+  /**
+   * {@link TableMaster} に登録されている選択クエリを全て取得する。
+   *
+   * @return recordset {@link TableMaster} の {@link List}
+   * @throws Exception {@link java.lang.Exception}
+   */
+  List<TableMaster> findQueryAll() throws Exception;
 
-    /**
-     * table_id をキーにクエリ定義を検索する。
-     *
-     * @param tableId
-     *            検索対象とする table_id を指定する。
-     * @return record table_id に紐づくテーブル定義を返す。削除フラグ 1 が設定されているレコードは除外する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public SelectDefinition findSelectDefinitionByTableId(int tableId)
-            throws Exception;
+  /**
+   * table_id をキーにクエリ定義を検索する。 削除フラグ 1 が設定されているレコードは除外する。
+   *
+   * @param tableId 検索対象とする table_id を指定する。
+   * @return record {@link SelectDefinition}
+   * @throws Exception {@link java.lang.Exception}
+   */
+  SelectDefinition findSelectDefinitionByTableId(int tableId) throws Exception;
 
-    /**
-     * クエリ定義を新規登録する。
-     *
-     * @param selectDefinition
-     *            登録対象とするクエリ定義を指定する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public void create(SelectDefinition selectDefinition) throws Exception;
+  /**
+   * クエリ定義を新規登録する。
+   *
+   * @param selectDefinition 登録対象とする {@link SelectDefinition} を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  void create(SelectDefinition selectDefinition) throws Exception;
 
-    /**
-     * クエリ定義を更新する。
-     *
-     * @param selectDefinition
-     *            更新対象とするクエリ定義を指定する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public void update(SelectDefinition selectDefinition) throws Exception;
+  /**
+   * クエリ定義を更新する。
+   *
+   * @param selectDefinition 更新対象とする {@link SelectDefinition} を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  void update(SelectDefinition selectDefinition) throws Exception;
 
-    /**
-     * table_id をキーにクエリ定義を削除する。
-     *
-     * @param tableId
-     *            削除対象とするクエリ定義の table_id を指定する。
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public void deleteByTableId(int tableId) throws Exception;
+  /**
+   * table_id をキーにクエリ定義を削除する。
+   *
+   * @param tableId 削除対象とするクエリ定義の table_id を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  void deleteByTableId(int tableId) throws Exception;
 
-    /**
-     * テーブル定義を全件削除する。
-     *
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public void deleteAll() throws Exception;
+  /**
+   * テーブル定義を全件削除する。
+   *
+   * @throws Exception {@link java.lang.Exception}
+   */
+  void deleteAll() throws Exception;
 }
